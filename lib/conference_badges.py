39be9a8410ab576ce672236325b0d2cd15edf2a5
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-10-09 01:43:54 +0300

    assignment completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
+# Function to create a badge message for a given name
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
+# Function to create a list of badge messages for a list of names
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
-def assign_rooms(names):
-    return None
+# Function to assign rooms to speakers and create a list of room assignments
+def assign_rooms(speakers):
+    room_assignments = []
+    for i, speaker in enumerate(speakers, start=1):
+        room_assignment = f"Hello, {speaker}! You'll be assigned to room {i}!"
+        room_assignments.append(room_assignment)
+    return room_assignments
 
+# Function to print badge messages and room assignments
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for badge in badges:
+        print(badge)
+    
+    for assignment in room_assignments:
+        print(assignment)
+
+# Example usage
+speakers = ["Arel", "Carol"]
+printer(speakers)
